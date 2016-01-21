938ffe2861bfed4f47028b44d92dcb3a89071bad
Julian Kwieciński <julkwiec@gmail.com>
2016-01-21 16:47:06 +0100

    Kilka usprawnień w logice gry

--- Pretwa.Gui/Program.cs
+++ Pretwa.Gui/Program.cs
@@ -26,61 +26,85 @@ namespace Pretwa.Gui
 
             while (true)
             {
-
                 do
                 {
                     ShowState(state, gui, nextmove);
-                    if (Board.hasPlayerLost(FieldState.NewColor(Player.Black), state) || Board.hasPlayerLost(FieldState.NewColor(Player.Red), state))
+                    if (Board.hasPlayerLost(FieldState.NewColor(Player.Black), state) ||
+                        Board.hasPlayerLost(FieldState.NewColor(Player.Red), state))
                     {
                         System.Windows.Forms.MessageBox.Show("Koniec gry");
                         return;
                     }
-                    string moveFrom = Microsoft.VisualBasic.Interaction.InputBox("Wybierz pionek", "Ruch etap 1", "", 0, 0);
-                    char[] pionek = moveFrom.ToCharArray();
-                    string moveTo = Microsoft.VisualBasic.Interaction.InputBox("Wybierz pole do przesunięcia pionka", "Ruch etap 2", "", 0, 0);
-                    char[] poleDocelowe = moveTo.ToCharArray();
 
-                    FieldCoords zrodlowe;
-                    FieldCoords docelowe;
-                    int pionekX = -1, pionekY = -1, poleDoceloweX = -1, poleDoceloweY = -1;
+                    Tuple<FSharpMap<FieldCoords, FieldState>, NextMove> nextState;
 
-                    //współrzędne:
-                    if (pionek[0] == 'C')
-                        zrodlowe = FieldCoords.Center;
-                    else {
-                        pionekX = Int32.Parse(pionek[0].ToString());
-                        pionekY = Int32.Parse(pionek[1].ToString());
-                        zrodlowe = FieldCoords.NewEdge(pionekX, pionekY);
+                    while (true)
+                    {
+                        try
+                        {
+                            string moveFrom = Microsoft.VisualBasic.Interaction.InputBox(
+                                "Wybierz pionek", "Ruch etap 1",
+                                "", 0, 0);
+                            char[] pionek = moveFrom.ToCharArray();
+
+                            string moveTo = Microsoft.VisualBasic.Interaction.InputBox(
+                                "Wybierz pole do przesunięcia pionka", "Ruch etap 2",
+                                "", 0, 0);
+                            char[] poleDocelowe = moveTo.ToCharArray();
+
+                            //współrzędne:
+                            FieldCoords zrodlowe;
+                            if (pionek[0] == 'C')
+                                zrodlowe = FieldCoords.Center;
+                            else
+                            {
+                                var pionekX = Int32.Parse(pionek[0].ToString());
+                                var pionekY = Int32.Parse(pionek[1].ToString());
+                                zrodlowe = FieldCoords.NewEdge(pionekX, pionekY);
+                            }
+
+                            FieldCoords docelowe;
+                            if (poleDocelowe[0] == 'C')
+                                docelowe = FieldCoords.Center;
+                            else
+                            {
+                                var poleDoceloweX = Int32.Parse(poleDocelowe[0].ToString());
+                                var poleDoceloweY = Int32.Parse(poleDocelowe[1].ToString());
+                                docelowe = FieldCoords.NewEdge(poleDoceloweX, poleDoceloweY);
+                            }
+                            nextState = Board.applyMove(zrodlowe, docelowe, state);
+                            break;
+                        }
+                        catch (Exception)
+                        {
+                            System.Windows.Forms.MessageBox.Show("Nieprawidłowy ruch!");
+                        }
                     }
 
-                    if (poleDocelowe[0] == 'C')
-                        docelowe = FieldCoords.Center;
-                    else {
-                        poleDoceloweX = Int32.Parse(poleDocelowe[0].ToString());
-                        poleDoceloweY = Int32.Parse(poleDocelowe[1].ToString());
-                        docelowe = FieldCoords.NewEdge(poleDoceloweX, poleDoceloweY);
-                    }
-                    Tuple<FSharpMap<FieldCoords, FieldState>, NextMove> tuple = Board.applyMove(zrodlowe, docelowe, state);
-                    state = tuple.Item1;
-                    nextmove = tuple.Item2;
-
+                    
+                    state = nextState.Item1;
+                    nextmove = nextState.Item2;
                 } while (nextmove.IsColor == false);
 
                 do
                 {
                     ShowState(state, gui, nextmove);
-                    if (Board.hasPlayerLost(FieldState.NewColor(Player.Black), state) || Board.hasPlayerLost(FieldState.NewColor(Player.Red), state))
+                    if (Board.hasPlayerLost(FieldState.NewColor(Player.Black), state))
                     {
-                        System.Windows.Forms.MessageBox.Show("Koniec gry");
+                        System.Windows.Forms.MessageBox.Show("Koniec gry. Zwyciężył gracz czerwony");
                         return;
                     }
+                    if (Board.hasPlayerLost(FieldState.NewColor(Player.Red), state))
+                    {
+                        System.Windows.Forms.MessageBox.Show("Koniec gry. Zwyciężył gracz czarny");
+                        return;
+                    } 
                     System.Windows.Forms.MessageBox.Show("Ruch komputera, wciśnij Enter");
                     Tuple<FSharpMap<FieldCoords, FieldState>, NextMove> tuple = Board.moveOfTheComputer(state, nextmove);
                     state = tuple.Item1;
                     nextmove = tuple.Item2;
 
                 } while (nextmove.IsColor == false); 
-
             }
 
         }
